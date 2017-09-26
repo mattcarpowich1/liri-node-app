39a87f6a95a90e83a90b32caefa618cc7c5a3f36
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2017-09-25 18:36:39 -0700

    Initial commit

---
+++ keys.js
@@ -0,0 +1,18 @@
+const twitterKeys = {
+  consumer_key: 'ExAzYzSjy92xAlLJMT9RIH40t',
+  consumer_secret: 'R4yePBHJh6OgqzkhgF2MALdW5kwcaOvxlmbbC0UIutD5OzmqXc',
+  access_token_key: '912131128848400389-QJscjCgh4o90TbEo033TdX2OBvJToaS',
+  access_token_secret: 'oUBInkDlRU2kis5AuVm0HakC52rTGllVaMrzTYRga6F7T'
+};
+
+const spotifyKeys = {
+	client_id: "4c566d09f7e341feb12b270c947287e3",
+	client_secret: "47936ca50ef74f7bae82a3dbb5360e7a"
+};
+
+const keys = {
+	spotify: spotifyKeys,
+	twitter: twitterKeys
+};
+
+module.exports = keys;
